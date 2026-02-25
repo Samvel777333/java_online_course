d75e4a43600b64a0da8e738340579d75cb35554f
Samvel777333 <samveldavtyan83@yahoo.com>
2026-02-25 13:35:49 -0500

    add Test.java

---
+++ src/Test.java
@@ -0,0 +1,34 @@
+public class Test {
+ static void main(String[] args) {
+  System.out.print("Barev\n");
+  System.out.println("Sirun");
+  byte x = 127;
+  System.out.println(x);
+  short y = -4542;
+  System.out.println(y);
+  int a = 454654;
+  System.out.println(a);
+  long b = 4465777777l;
+  System.out.println(b);
+  float aa = 45.4F;
+  double bb = 45.4;
+  boolean v = true;
+  System.out.println(v);
+  v = false;
+  System.out.println(v);
+  char t = '$';
+  System.out.println(t);
+  char tt = '\u003F';
+  System.out.println(tt);
+  String s = "barev sirun";
+  System.out.println(s);
+  String firstName = "samvel";
+  String lastName = "Davtyan";
+  System.out.println(firstName +" " +lastName);
+  int k = 95;
+  System.out.println("k = " + k);
+
+
+
+ }
+}
